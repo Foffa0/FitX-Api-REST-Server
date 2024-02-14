999633efbfb44b4d531321d710cea1d811bf41f9
Foffa <schmuck.christopher@gmail.com>
2024-02-14 19:56:33 +0100

    Add rate limiter DDOS protextion

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import { errorHandler } from './middleware/errorMiddleware.js';
-import { runUpdateSchedule } from'./jobs/studioUpdater.js';
+import { runUpdateSchedule } from'./services/studioUpdater.js';
 import { connectDB } from'./config/db.js';
 import { studioRouter } from './routes/studioRoutes.js';
 import { capacityRouter } from './routes/capacityRoutes.js';
+import { rateLimiterMiddleware } from './middleware/rateLimiter.js';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+app.use(rateLimiterMiddleware);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
